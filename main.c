4ce62504254c7c2acbacb2e32fc3c25298be7199
eestelle <yosiky@list.ru>
2021-09-01 09:55:46 +0300

    changed main.c

--- main.c
+++ main.c
@@ -9,10 +9,12 @@
 
 #define INF	2147483647
 
-int			heigth_window = 480; //размер окна
-int			width_window = 640;
-int			Ch = 1080; //разрешение
-int			Cw = 1920;
+float		eps = 0.0000001;
+int			Vh = 1; // set in 1x1x1
+int			Vw = 1;
+int			Vd = 1;
+int			Ch = 600; //разрешение
+int			Cw = 600;
 int			count_figure;
 t_sphere	**spheres;
 t_window	*window;
@@ -20,99 +22,92 @@ t_window	*window;
 void	putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
 void	ee_print_color(Uint32 color);
 
-void	canvastoviewport(t_point3d *point, float x, float y) 
+void	canvastoviewport(t_point3d *d, float x, float y)
 {
-	ee_set_point(point, x / width_window * Cw, y / heigth_window * Ch, 1);
+	ee_set_point(d, x * Vw / Cw, y * Vh / Ch, Vd);
 }
 
-void	itersectraysphere(t_point3d *o, t_point3d *d, t_sphere *figure, float *res)
+void	intersectraysphere(t_point3d *o, t_point3d *d, t_sphere *s, float res[2])
 {
+	t_point3d	oc;
+	float		r;
 	float		k1;
 	float		k2;
 	float		k3;
-	float		discriminant;
-	t_point3d	oc;
+	float		disc;
 
-	ee_point_minus(&oc, o, &figure->center);
-	printf("\noc = ");
-	ee_point_print(&oc);
-	printf("\nd = ");
-	ee_point_print(d);
-	//ee_point_norm(&oc);
-	printf("\nnoc = ");
-	ee_point_print(&oc);
-	*res = INF;
+	res[0] = INF;
+	res[1] = INF;
+	ee_point_minus(&oc, o, &(s->center));
+	r = s->radius;
 	k1 = ee_point_dot(d, d);
 	k2 = 2 * ee_point_dot(&oc, d);
-	k3 = ee_point_dot(&oc, &oc) - figure->radius * figure->radius;
-	discriminant = k2 * k2 - 4 * k1 * k3;
-	printf("%f %f %f dis = %f\n", k1, k2, k3, discriminant);
-	if (discriminant >= 0)
-	{
-		if (k2 >= 0)
-			*res = (-k2 + sqrt(discriminant)) / (2 * k1);
-		else
-			*res = (-k2 - sqrt(discriminant)) / (2 * k1);
-	}
-	printf("end iterspheres\n");
+	k3 = ee_point_dot(&oc, &oc) - r * r;
+	disc = k2 * k2 - 4 * k1 * k3;
+	if (disc < 0)
+		return ;
+	res[0] = (-k2 - sqrt(disc)) / (2 * k1);
+	res[1] = (-k2 + sqrt(disc)) / (2 * k1);
 }
 
-Uint32	traceray(t_point3d *o, t_point3d *d, float minimal, float maximal)
+Uint32	traceray(t_point3d	*o, t_point3d *d, float minimal, float maximal)
 {
-	t_sphere	*iter;
-	float		len;
-	float		res;
+	float	len;
+	float	res[2];
+	Uint32	color;
 
-	len = maximal;
-	iter = (void *)0;
-	printf("figures:\n");
+	color = window->background;
 	for (int i = 0; i < count_figure; i++)
 	{
-		itersectraysphere(o, d, spheres[i], &res);
-		if (minimal <= res && res <= maximal && res < len)
+		intersectraysphere(o, d, spheres[i], res);
+		for (int j = 0; j < 2; j++)
 		{
-			len = res;
-			iter = spheres[i];
-		}	
-	}	
-	if (iter == (void *)0)
-		return (window->background);
-	printf("end figures\n");
-	return (iter->color);
+			if (minimal <= res[j] && res[j] <= maximal && res[j] < len)
+			{
+				len = res[j];
+				color = spheres[i]->color;
+			}
+		}
+	}
+	return (color);
 }
 
-void	setcanvas(t_window *window)
+void	setcanvas(SDL_Surface *surface)
 {
-	SDL_Surface	*surface;
+	t_point3d	o;
 	t_point3d	d;
-	t_point3d	o;	
 	Uint32		color;
-	
+
 	ee_set_point(&o, 0, 0, 0);
-	surface = SDL_GetWindowSurface(window->window);
-	printf("Start to round\n");
-	for (int i = -window->width / 2; i <= window->width / 2; i++)
+	for	(int i = -Cw / 2; i <= Cw / 2; i++)
 	{
-		for (int j = -window->heigth / 2; j <= window->heigth / 2; j++)
+		//printf("Start:\n");
+		for (int j = -Ch / 2; j <= Ch / 2; j++)
 		{
-			printf("\ti = %d, j = %d\n", i, j);
 			canvastoviewport(&d, i, j);
 			color = traceray(&o, &d, 1, INF);
-			ee_print_color(color);
-			printf("\n\n");
-			putpixel(surface, i + window->width / 2, j + window->heigth / 2, color);
+			//printf("\ti = %d, j = %d\n\t\t", i, j);
+			//ee_print_color(color);
+			//printf("\n");
+			putpixel(surface, i + Cw / 2, Ch / 2 - j, color);
 		}
-	} 
-	printf("End rount\n");
+		//print1f("End\n\n");
+	}
 }
 
 void	start(t_window *window)
 {
+	SDL_Event	e;
+
+	setcanvas(SDL_GetWindowSurface(window->window));	
+	SDL_UpdateWindowSurface(window->window);
 	while (1)
 	{
-		setcanvas(window);	
-		SDL_UpdateWindowSurface(window->window);
-		break;
+		SDL_PollEvent(&e);
+		if (e.type == SDL_QUIT) {
+			SDL_Log("Program quit after %i ticks", e.quit.timestamp);
+			break;
+		}
 	}
 }
 
@@ -126,11 +121,21 @@ void	delete(void	**figures, int n)
 int	main(int argc, char **argv)
 {
 	Uint32	color;
+	t_point3d	point;
 	
 	color = 0;
 	color |= 127;
 	color |= 127 << 8;
 	color |= 127 << 16;
+	count_figure = 1;
+	spheres = (t_sphere **)malloc(sizeof(t_sphere *) * count_figure);
+	ee_set_point(&point, 0, -1, 3);
+	spheres[0] = ee_create_sphere(&point, 1, 255<<16);
+	//ee_set_point(&point, 10, 10, 10);
+	//spheres[1] = ee_create_sphere(&point, 2, 255<<8);
+	ee_sphere_print(spheres[0]);
+	//ee_sphere_print(spheres[1]);
+
 	if (argc && argv)
 		write(1, "Start program\n", 14);
 	SDL_Surface	*window_surface;
@@ -140,7 +145,7 @@ int	main(int argc, char **argv)
 		printf("Error in SDL_Init\n");
 		return (0);
 	}
-	window = ee_create_window(width_window, heigth_window, color);	
+	window = ee_create_window(Cw, Ch, color);	
     if (!window->window)
 	{
 		printf("Failed to create window\n");
@@ -153,19 +158,10 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	ee_window_print(window);
-	t_point3d	point;
-	count_figure = 1;
-	spheres = (t_sphere **)malloc(sizeof(t_sphere *) * count_figure);
-	ee_set_point(&point, 0, -1, 3);
-	spheres[0] = ee_create_sphere(&point, 1, 255<<16);
-	//ee_set_point(&point, 10, 10, 10);
-	//spheres[1] = ee_create_sphere(&point, 2, 255<<8);
-	ee_sphere_print(spheres[0]);
-	//ee_sphere_print(spheres[1]);
 	start(window);
-	SDL_Delay(5000);
-	ee_delete_window(window);
+	//SDL_Delay(5000);
 	printf("End programm\n");
-	delete((void **)spheres, count_figure);
+	ee_delete_window(window);
+	//delete((void **)spheres, count_figure);
 	return (0);
 }
